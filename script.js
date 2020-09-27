95678f3eeda9f7811dd41e4a7a5624686955a719
E2050-FARUK <68036139+E2050-FARUK@users.noreply.github.com>
2020-09-27 18:49:41 +0300

    update css and js

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-document.getElementById("check").addEventListener(
-    "click", checkNumber)
-    let counter = 0;
-    const myNum = Math.floor(Math.random() * 101);
-    function checkNumber(){
-        let guessNum = document.getElementById("guess").value;
-        counter += 1;
-        document.getElementById("counter").innerHTML = "This is your test number:" + counter;
-        if (guessNum == ""){
-            document.getElementById("answer").innerHTML = "Write a number?";
-            
-        } else if (isNaN(guessNum)) {
-            document.getElementById("answer").innerHTML = "Are you sure that this is a number?";
-            
-        } else if (guessNum < myNum){
-            document.getElementById("answer").innerHTML = "I am thinking about higher number";
-            
-        } else if (guessNum > myNum) {
-            document.getElementById("answer").innerHTML = "I am thinking about lower number";
-            
+
+let counter = 0;
+const myNum = Math.floor(Math.random() * 100 + 1);
+document.getElementById("check").addEventListener("click", checkNumber)    
+document.querySelector("#guess").addEventListener("keyup", function(event){
+    if (event.keyCode === 13){
+        checkNumber()
+    }
+});
+
+function checkNumber(){
+    let guessNum = document.getElementById("guess").value;
+    counter += 1;
+    document.getElementById("counter").innerHTML = "This is your test number: " + counter;
+    if (guessNum == ""){
+        document.getElementById("answer").innerHTML = "Write a number?";
+        
+    } else if (isNaN(guessNum)) {
+        document.getElementById("answer").innerHTML = "Are you sure that this is a number?";
+        
+    } else if (guessNum < myNum){
+        document.getElementById("answer").innerHTML = "I am thinking about higher number";
+        
+    } else if (guessNum > myNum) {
+        document.getElementById("answer").innerHTML = "I am thinking about lower number";
+        
+    }
+    else {
+        
+        alert("You are the winner 😃 " + "You guessed after " + counter + " times!" );
+        if (window.confirm("You wanna play again!")) {
+            window.location.href = "./index.html"
         }
         else {
-            alert("You are the winner 😃 " + "You guessed after " + counter + " times!" );
-            if (window.confirm("You wanna play again!")) {
-                window.location.href = "./index.html"
-            }
-            else {
-                document.getElementById("answer").innerHTML = "Thanks for playing game.";
-                document.getElementById("counter").innerHTML = "";
-            }
+            document.getElementById("answer").innerHTML = "Thanks for playing game.";
+            document.getElementById("counter").innerHTML = "";
         }
-    document.getElementById("guess").value='';
     }
+    document.getElementById("guess").focus();
+    document.getElementById("guess").value='';
+}
